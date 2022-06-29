612170862c1687cbb70d504e29a28a8cfb635e0a
krynak <krystian.nakielski200397@gmail.com>
2022-06-29 14:34:23 +0200

    Initial commit

---
+++ src/main/kotlin/com/mibi/ThesisPrototypeApplication.kt
@@ -0,0 +1,11 @@
+package com.mibi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThesisPrototypeApplication
+
+fun main(args: Array<String>) {
+    runApplication<ThesisPrototypeApplication>(*args)
+}
