44ba0288aac50b9e809311e66550bba1cf004653
krynak <krystian.nakielski200397@gmail.com>
2022-06-29 14:41:54 +0200

    Added confirmation endpoint

---
+++ src/main/kotlin/com/mibi/ConfirmationController.kt
@@ -0,0 +1,19 @@
+package com.mibi
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(path = ["/confirmation"])
+class ConfirmationController {
+
+    @GetMapping(path = ["/"])
+    @ResponseStatus(HttpStatus.OK)
+    fun index(): String{
+        return "Success"
+    }
+
+}
